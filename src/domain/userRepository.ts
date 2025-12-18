eca5d2c85e1abf4a6d6626ea2d7568d11c53e1c5
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2025-12-18 12:13:48 +0100

    feat: add user Repository

--- src/domain/userRepository.ts
+++ src/domain/userRepository.ts
@@ -2,13 +2,4 @@ import User from "./Entities/user"
 
 export interface UserRepository {
   save(user: User): Promise<void>
-  delete(user: User): Promise<void>
-}
-
-export class UserDB {
-  static save(user: User) {
-    User.find(user.id).then((id) => {
-      id.update((user.id, user))
-    })
-  }
 }
