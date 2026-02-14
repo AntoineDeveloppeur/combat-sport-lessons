10cef31c3c05dd70cddd69f385c90619aa8ac07f
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2026-02-14 17:32:47 +0100

    refactor: isole la validation du schema pour les instructions avec un module à part entière

--- frontend/hooks/useInstructionForm.ts
+++ frontend/hooks/useInstructionForm.ts
@@ -11,6 +11,7 @@ import * as Yup from "yup"
 import { LessonContext } from "@/app/provider"
 import { useRouter } from "next/navigation"
 import { UseFormHandleSubmit, UseFormRegister } from "react-hook-form"
+import { yupValidationSchema } from "@/utils/instructionValidationSchema"
 
 interface UseInstructionFormConfig {
   fieldName: "warmUpInstructions" | "bodyInstructions" | "coolDownInstructions"
@@ -43,26 +44,7 @@ export function useInstructionForm({
   )
 
   // Validation schema (identique pour tous)
-  const validationSchema = Yup.object().shape({
-    [fieldName]: Yup.array()
-      .of(
-        Yup.object().shape({
-          text: Yup.string()
-            .min(3, "L'instruction doit contenir au moins 3 caractères")
-            .max(2000, "L'instruction ne peut pas dépasser 2000 caractères")
-            .required("L'instruction est requise"),
-          min: Yup.number()
-            .min(0, "Les minutes doivent être au minimum 0")
-            .max(90, "Les minutes ne peuvent pas dépasser 90")
-            .required("Les minutes sont requises"),
-          sec: Yup.number()
-            .min(0, "Les secondes doivent être au minimum 0")
-            .max(59, "Les secondes ne peuvent pas dépasser 59")
-            .required("Les secondes sont requises"),
-        })
-      )
-      .required(),
-  })
+  const validationSchema = yupValidationSchema(fieldName)
 
   type FormData = Yup.InferType<typeof validationSchema>
 
---
+++ frontend/utils/instructionValidationSchema.ts
@@ -0,0 +1,28 @@
+import * as Yup from "yup"
+import { type Lesson } from "@/types"
+
+export function yupValidationSchema(
+  fieldName: keyof Lesson
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Yup.ObjectSchema<any> {
+  return Yup.object().shape({
+    [fieldName]: Yup.array()
+      .of(
+        Yup.object().shape({
+          text: Yup.string()
+            .min(3, "L'instruction doit contenir au moins 3 caractères")
+            .max(2000, "L'instruction ne peut pas dépasser 2000 caractères")
+            .required("L'instruction est requise"),
+          min: Yup.number()
+            .min(0, "Les minutes doivent être au minimum 0")
+            .max(90, "Les minutes ne peuvent pas dépasser 90")
+            .required("Les minutes sont requises"),
+          sec: Yup.number()
+            .min(0, "Les secondes doivent être au minimum 0")
+            .max(59, "Les secondes ne peuvent pas dépasser 59")
+            .required("Les secondes sont requises"),
+        })
+      )
+      .required(),
+  })
+}
