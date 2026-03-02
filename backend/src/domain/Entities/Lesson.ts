9cc5d3bbd4e21afc5db0e0f528b288a31138ed88
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2026-03-02 12:34:26 +0100

    feat: met à jour l'entities lesson

--- backend/src/domain/Entities/Lesson.ts
+++ backend/src/domain/Entities/Lesson.ts
@@ -3,11 +3,14 @@ import { Instruction } from "./Instructions"
 
 export class Lesson {
   constructor(
+    public readonly lessonId: number,
+    public readonly title: string,
     public readonly sport: Sport,
     public readonly objective: string,
-    public readonly duration: number,
-    public readonly coreInstructions: Instruction[],
     public readonly warmUpInstructions: Instruction[],
-    public readonly coolDownInstructions: Instruction[]
+    public readonly bodyInstructions: Instruction[],
+    public readonly coolDownInstructions: Instruction[],
+    public readonly creationDate: Date,
+    public readonly userID: number
   ) {}
 }
