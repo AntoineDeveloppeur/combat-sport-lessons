a80a62a6334d66d9783bacec32fc122a61d12893
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2026-03-04 17:49:24 +0100

    feat: propage la lesson fetch dans le formulaire avec dispatch

--- frontend/components/form/FetchLessonFromBackend.tsx
+++ frontend/components/form/FetchLessonFromBackend.tsx
@@ -1,9 +1,12 @@
 import { Button } from "../ui/button"
 import { useState, useEffect } from "react"
 import { useGetLessonQuery } from "@/store/api/lessonApi"
-
+import { useAppDispatch } from "@/store/hooks"
+import { save } from "@/features/lesson/lessonSlice"
 export default function FetchLessonFromBackend() {
   const [shouldFetch, setShouldFetch] = useState(false)
+  const dispatch = useAppDispatch()
+
   const { data, isLoading, error } = useGetLessonQuery(1, {
     skip: !shouldFetch, // Ne pas exécuter la query si shouldFetch est false
   })
@@ -14,6 +17,7 @@ export default function FetchLessonFromBackend() {
 
   useEffect(() => {
     if (data?.lesson) {
+      dispatch(save(data.lesson))
       console.log("Données reçues:", data.lesson)
     }
     console.log("État actuel:", { data, isLoading, error })
