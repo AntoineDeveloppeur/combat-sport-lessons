3cac554d15326413470ef14a0d5d65f8884b61f1
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2026-03-14 10:02:48 +0100

    feat: utilise une variable d'envrionnement pour nourrir l'api lesson

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import { config } from "dotenv"
+import { resolve } from "path"
+
+// Charger le .env du dossier parent (racine du projet)
+config({ path: resolve(__dirname, "../.env") })
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.BACKEND_URL,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
