ed8eb1844fc115a7e04f1ce3802dd823ce2ef519
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2026-03-19 11:57:27 +0100

    test: ajoute un timeout avant de vérifier si un élément est visible

--- tests/integration/lesson-download.spec.ts
+++ tests/integration/lesson-download.spec.ts
@@ -9,12 +9,16 @@ test("should download lesson when clicking button", async ({ page }) => {
 
   await page.click('button:has-text("Récupérer la lesson")')
 
-  await expect(page.locator('button:has-text("Chargement...")')).toBeVisible()
+  await expect(page.locator('button:has-text("Chargement...")')).toBeVisible({
+    timeout: 5000,
+  })
 
   await expect(page.locator("text=Leçon récupérée")).toBeVisible({
     timeout: 20000,
   })
 
   // check if there is a caracter after ":"
-  await expect(page.locator("text=/Leçon récupérée : .+/")).toBeVisible()
+  await expect(page.locator("text=/Leçon récupérée : .+/")).toBeVisible({
+    timeout: 5000,
+  })
 })
