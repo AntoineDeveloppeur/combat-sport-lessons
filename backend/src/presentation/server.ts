3d7e4084f7e221388a930e8c9c6deb411c0656c7
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2026-04-28 15:46:56 +0200

    refactor: supprimer les console log de debuggage

--- backend/src/presentation/server.ts
+++ backend/src/presentation/server.ts
@@ -5,6 +5,4 @@ const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`✅ Serveur démarré sur le port ${port}`)
-  console.log(`📚 Documentation Swagger: http://localhost:${port}/api-docs`)
-  console.log(`📄 Spec OpenAPI JSON: http://localhost:${port}/api-docs.json`)
 })
